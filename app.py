c1e30be2413f557af5d37da84b2c5e96cd00ad38
Yash Raorane <yraoran@clemson.edu>
2024-12-01 19:09:28 -0500

    adding ports

--- app.py
+++ app.py
@@ -119,4 +119,7 @@ def recommend():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
