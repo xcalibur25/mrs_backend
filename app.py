8a819a87650b71f4fde473cb67bcdb2b7efe0f03
Yash Raorane <yraoran@clemson.edu>
2024-12-04 11:56:11 -0500

    removing unwanted changes

--- app.py
+++ app.py
@@ -129,7 +129,5 @@ def recommend():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    #if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5003)
 
